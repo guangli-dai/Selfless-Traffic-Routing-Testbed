edba5aa51aa9b1056a9128303b949e2c885b1487
tcrybicki <40121695+tcrybicki@users.noreply.github.com>
2020-02-25 21:51:47 -0600

    make_decision -> make_decisions
    the decision function now takes batches of vehicles rather than individual ones

--- RouteController.py
+++ RouteController.py
@@ -11,19 +11,27 @@ SLIGHT_LEFT = "L"
 SLIGHT_RIGHT = "R"
 
 class RouteController(ABC):
-    '''
+    """
     Base class for routing policy
-    '''
+    """
     def __init(self, connection_info):
         self.connection_info = connection_info
         self.direction_choices = [STRAIGHT, TURN_AROUND,  SLIGHT_RIGHT, RIGHT, SLIGHT_LEFT, LEFT]
     @abstractmethod
-    def make_decision(self, vehicle_id):
+    def make_decisions(self, vehicles, vehicle_id):
         pass
 
 class RandomPolicy(RouteController):
+    """
+    :param connection_info: object containing network information including
+
+    """
     def __init__(self, connection_info):
         super().__init(connection_info)
 
-    def make_decision(self, vehicle_id):
-        return random.randint(0, 6)
+    def make_decisions(self, vehicles, connection_info):
+        decisions = {}
+        for vehicle in vehicles:
+            decisions[vehicle.vehicle_id] = self.direction_choices[random.randint(0, 6)]
+
+        return decisions
